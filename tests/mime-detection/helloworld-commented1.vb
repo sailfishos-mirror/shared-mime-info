5e7302565594b012cffa623842d7f2ef25aebc63
Andrew Chadwick <1750-achadwick@users.noreply.gitlab.freedesktop.org>
2026-06-24 01:19:26 +0100

    VB.net: improve file magic
    - Require trailing space in keywords
    - More flexible magic that matches at line starts (primarily) too
      - keywords which are likely to be found there, near the start
      - still need magic to beat un-magicable Virtual Boy ROMs
    - Remove REM keyword, though:
      - too many false positives: see #250 and comment in !407
      - "rem" is not even the most common VB comment syntax
      - chance of misdetecting DOS batch files or remind files as VB
    - Add some more test cases to make sire leading comment lines
      don't break detection
    
    closes #250

---
+++ tests/mime-detection/helloworld-commented1.vb
@@ -0,0 +1,7 @@
+REM Classic "hello world" in Visual Basic
+
+Module HelloWorld
+    Sub Main()
+        System.Console.WriteLine("Hello World!")
+    End Sub
+End Module
---
+++ tests/mime-detection/helloworld-commented2.vb
@@ -0,0 +1,7 @@
+' Classic "hello world" in Visual Basic
+
+Module HelloWorld
+    Sub Main()
+        System.Console.WriteLine("Hello World!")
+    End Sub
+End Module
